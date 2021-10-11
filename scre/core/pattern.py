9cfb06a7705502b15c1cface4806c4190fa21368
jstzwj <1103870790@qq.com>
2021-10-11 14:51:21 +0800

    update

--- scre/core/pattern.py
+++ scre/core/pattern.py
@@ -1,5 +1,11 @@
-
+from scre.core.pattern import Pattern
+from scre.parser.parser import Parser
 
 class Pattern:
     def __init__(self) -> None:
         pass
+
+    def search(self, pattern: str, text: str) -> Pattern:
+        parser = Parser(pattern)
+        ast = parser.parse()
+        print(parser.diagnostic)
